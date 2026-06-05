3a74613c57199cf27f7d21992bc75ae39b59f6cd
vilo77 <55990526+vilo77@users.noreply.github.com>
2026-06-05 10:10:59 +0700

    Add logging configuration and initialize logger

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,16 @@
 """
 Modules package — auto-discovered by eleger/loader.py
 """
+import logging
+
+# Inisialisasi Logger agar tidak error lagi
+logger = logging.getLogger("eleger")
+
+# Variabel Global untuk Assistant Bot
+assistant = None
+
+# Konfigurasi level logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+)
